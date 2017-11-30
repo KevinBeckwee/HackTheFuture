1579251d74001b40411ab31eff3b1a199df3f3d5
KevinBeckwee <medabots4@hotmail.com>
2017-11-30 13:15:03 +0100

    login test
    first login attempt

--- HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
+++ HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
@@ -4,18 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     TextView tvCardText;
     Button btStartScan;
+    private String ACCES_CODE;
+    private final String URL = "http://37.230.72/htf/api/";
+    RequestQueue queue;
 
 
 
@@ -34,6 +52,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startQRScanner();
             }
         });
+         queue = Volley.newRequestQueue(this);
+
     }
 
 
@@ -59,10 +79,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void updateText(String scanCode) {
         tvCardText.setText(scanCode);
+        ACCES_CODE = scanCode;
     }
 
     @Override
     public void onClick(View view) {
-        startQRScanner();
+        try {
+            login();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void login() throws JSONException {
+        JSONObject heyboo = new JSONObject();
+        heyboo.put("qrCode",ACCES_CODE);
+        JsonObjectRequest req = new JsonObjectRequest(URL, heyboo, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    VolleyLog.v("Response:%n %s", response.toString(4));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+        queue.add(req);
+
+
+
     }
 }
