da0ffd14c8c58cdac75e983725b2e89782fb5f80
KevinBeckwee <medabots4@hotmail.com>
2017-11-30 15:58:39 +0100

    werkende mines

--- HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
+++ HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
@@ -3,16 +3,19 @@ package com.example.keiichi.hackthefuturemobile;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     TextView tvCardText;
     Button btStartScan;
+    Button btnMines;
     private String QRCODE;
     private String ACCES_Code;
     private final String URL = "http://37.230.98.72/htf/api/";
@@ -42,14 +46,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         tvCardText = (TextView)findViewById(R.id.tv_code_text);
         btStartScan = (Button)findViewById(R.id.btn_scan);
+        btnMines = findViewById(R.id.mineBtn);
 
-        btStartScan.setOnClickListener(new View.OnClickListener() {
+        btnMines.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startQRScanner();
+                try {
+                    getMines();
+                } catch (AuthFailureError authFailureError) {
+                    authFailureError.printStackTrace();
+                }
+
             }
         });
+        
          queue = Volley.newRequestQueue(this);
+         startQRScanner();
 
     }
 
@@ -115,8 +127,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         queue.add(jsonobj);
 
+    }
 
+    public void getMines() throws AuthFailureError{
+        /*Map<String, String> params = new HashMap<>();
+        params.put("Authorization", "Bearer " + ACCES_Code);
+        JSONObject jsonObject = new JSONObject(params);
+        JsonObjectRequest postRequest = new JsonObjectRequest(Request.Method.GET, URL + "mines",
+                jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    System.out.println(response.getJSONArray("Beaulieu"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }){
+            public Map<String,String> getHeaders(){
+             Map<String, String> params = new HashMap<>();
+             params.put("Authorization", "Bearer " + ACCES_Code);
 
+             return params;
+            }
+        };
 
+        queue.add(postRequest);*/
+        StringRequest postRequest = new StringRequest(Request.Method.GET, URL + "mines", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                }){
+
+            public Map<String,String> getHeaders(){
+                Map<String,String> params = new HashMap<>();
+                params.put("Authorization", "Bearer "+ ACCES_Code);
+
+                return params;
+            }
+
+        };
+        queue.add(postRequest);
     }
 }
