bc14d9918fbc27544b163bf2de36e42dfa0f540a
KevinBeckwee <medabots4@hotmail.com>
2017-11-30 10:51:00 +0100

    barcode start
    start van QR scanner

--- HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
+++ HackTheFutureMobile/app/src/main/java/com/example/keiichi/hackthefuturemobile/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.keiichi.hackthefuturemobile;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    SurfaceView cameraView;
+    TextView barcodeInfo;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.camera_view);
+        barcodeInfo = findViewById(R.id.code_info);
+        barcodeDetector =
+                new BarcodeDetector.Builder(this)
+                        .setBarcodeFormats(Barcode.QR_CODE)
+                        .build();
+
+        cameraSource = new CameraSource
+                .Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ie) {
+                    Log.e("CAMERA SOURCE", ie.getMessage());
+                }
+                barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+                    @Override
+                    public void release() {
+                    }
+
+                    @Override
+                    public void receiveDetections(Detector.Detections<Barcode> detections) {
+                        final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                        if (barcodes.size() != 0){
+                            barcodeInfo.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    barcodeInfo.setText(
+                                            barcodes.valueAt(0).displayValue
+                                    );
+
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
     }
+
 }
+
